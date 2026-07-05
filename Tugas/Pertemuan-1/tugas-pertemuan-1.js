f90f0094a0b4be818f2af5c489f36ebd67884449
Aprill33 <aprillee344@gmail.com>
2026-07-05 15:50:36 +0700

    menggunakan spread operator untuk menambahkan properti baru tanpa mengubah object asli

--- Tugas/Pertemuan-1/tugas-pertemuan-1.js
+++ Tugas/Pertemuan-1/tugas-pertemuan-1.js
@@ -5,9 +5,9 @@ const mahasiswa = {
     semester: 4,
     nilai: [85, 90, 78, 88]
 };
-
+// destructuring assignment untuk mengamabil data dari object mahasiswa
 const {nama, nim, prodi, semester, nilai} = mahasiswa;
-
+// arrow function untuk menghitung nilai rata-rata mahasiswa
 const rataRata = (nilai) => {
     let total = 0;
 
@@ -17,4 +17,8 @@ const rataRata = (nilai) => {
 
     return total / nilai.length;
 }
-
+// menambahkan properti baru pada object mahasiswa menggunakan spread operator
+const dataLengkapMhs ={
+    ...mahasiswa,
+    status : "aktif"
+}
