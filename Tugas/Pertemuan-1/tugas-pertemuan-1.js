5201b7989cf4903a66dde8974411cc7ffc05d266
Aprill33 <aprillee344@gmail.com>
2026-07-05 16:08:50 +0700

    mengunakan rest operator di parameter function untuk menghitung total nilai

--- Tugas/Pertemuan-1/tugas-pertemuan-1.js
+++ Tugas/Pertemuan-1/tugas-pertemuan-1.js
@@ -5,8 +5,16 @@ const mahasiswa = {
     semester: 4,
     nilai: [85, 90, 78, 88]
 };
+
+// menambahkan properti baru pada object mahasiswa menggunakan spread operator
+const dataLengkapMhs ={
+    ...mahasiswa,
+    status : "aktif"
+}    
+
 // destructuring assignment untuk mengamabil data dari object mahasiswa
 const {nama, nim, prodi, semester, nilai} = mahasiswa;
+
 // arrow function untuk menghitung nilai rata-rata mahasiswa
 const rataRata = (nilai) => {
     let total = 0;
@@ -16,9 +24,16 @@ const rataRata = (nilai) => {
     }
 
     return total / nilai.length;
+}    
+
+// menggunakan rest operator di parameter function untuk menghitung total nilai mahasiswa
+const totalNilai = (...angka) => {
+    let total = 0;
+
+    for (let n of angka){
+        total += n;
+    }
+
+    return total;
 }
-// menambahkan properti baru pada object mahasiswa menggunakan spread operator
-const dataLengkapMhs ={
-    ...mahasiswa,
-    status : "aktif"
-}
+
